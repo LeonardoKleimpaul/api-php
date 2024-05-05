6fafc20e38c7b03a7b014d500c1b5822eac043a8
Leonardo Kleimpaul <leonardokleimpaul@protonmail.com>
2024-05-05 02:05:44 -0300

    ajustando cors

--- app/application/Util/JsonUtil.php
+++ app/application/Util/JsonUtil.php
@@ -35,6 +35,7 @@ class JsonUtil
 
     private function retornaJson($dados)
     {
+        header('Access-Control-Allow-Origin: *');
         header('Content-Type: application/json');
         header('Cache-Control: no-cache, no-store, must-revalidate');
         header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
