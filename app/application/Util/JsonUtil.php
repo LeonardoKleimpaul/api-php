858a29d3db646015b8c3aea80608b0b8d0c50a42
Leonardo Kleimpaul <leonardokleimpaul@gmail.com>
2024-05-05 10:14:47 -0300

    Configuracao de cors

--- app/application/Util/JsonUtil.php
+++ app/application/Util/JsonUtil.php
@@ -35,10 +35,30 @@ class JsonUtil
 
     private function retornaJson($dados)
     {
-        header('Access-Control-Allow-Origin: *');
+        // Permitir de qualquer origem
+        if (isset($_SERVER['HTTP_ORIGIN'])) {
+            header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+            header('Access-Control-Allow-Credentials: true');
+            header('Access-Control-Max-Age: 86400');
+        }
+
+        // Acessar os headers da requisição pre-flight OPTIONS
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+                // Pode ser um método HTTP personalizado
+                header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+                // Pode ser um header personalizado
+                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+            exit(0);
+        }
+
+        // Definir o tipo de conteúdo para JSON
         header('Content-Type: application/json');
-        header('Cache-Control: no-cache, no-store, must-revalidate');
-        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
         echo json_encode($dados);
         exit;
     }
